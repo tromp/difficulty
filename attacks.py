4ad3b8afa2b24a3002fade08665cbcac7a30449e
Jonathan Toomim <j@toom.im>
2020-07-27 20:32:57 -0700

    Add some grasberg attacks

--- attacks.py
+++ attacks.py
@@ -12,28 +12,35 @@ import mining
 
 sys.setrecursionlimit(100000)
 
-worstcases = {'asert':2.5*log(2),
-              'aserti3':2.5*log(2)}
-
 params = {'INITIAL_BCC_BITS':0x18084bb7,
         'INITIAL_TIMESTAMP':1503430225,
         'INITIAL_HEIGHT':481824}
 
-def fast_forward_attack(algoname, forward_blocks=1, total_blocks=500, total_jump=None, FTL=7200, autoend=False):
-  algo = mining.Algos[algoname]
-  if not total_blocks:
-    try:
-      algtype = algoname.split('-')[0]
-      N = algoname.split('-')[-1]
-      if algtype in worstcases:
-        total_blocks = int(N * worstcases['algtype'])
-      else:
-        total_blocks = int(N * 2.5*log(2))
-    except:
-      pass
-  if not total_jump:
+def fast_forward_selector(forward_blocks, total_blocks, FTL):
+  def selector(states, n):
     total_jump = total_blocks*mining.IDEAL_BLOCK_TIME + FTL
+    if n < forward_blocks:
+      return states[-1].timestamp + total_jump // forward_blocks
+    else:
+      return states[-1].timestamp + 1
+  return selector
 
+def two_week_jump_selector(forward_blocks, total_blocks, FTL):
+  def selector(states, n):
+    #total_jump = 14 * 24 * 3600*1
+    total_jump = total_blocks*mining.IDEAL_BLOCK_TIME + FTL
+    if n % 2:
+      return states[-1].timestamp - (total_jump - mining.IDEAL_BLOCK_TIME*n)
+    else:
+      return states[-1].timestamp + (total_jump - mining.IDEAL_BLOCK_TIME*n) + mining.IDEAL_BLOCK_TIME
+  return selector
+
+
+def simulate_attack(algoname, selector_type, forward_blocks=1, total_blocks=500, FTL=7200, autoend=False):
+  algo = mining.Algos[algoname]
+
+  total_jump = total_blocks*mining.IDEAL_BLOCK_TIME + FTL
+  selector = selector_type(forward_blocks, total_blocks, FTL)
   states = []
   mining.states = states # ugh
   N = 2020
@@ -53,10 +60,7 @@ def fast_forward_attack(algoname, forward_blocks=1, total_blocks=500, total_jump
     target = mining.bits_to_target(bits)
     chainwork = states[-1].chainwork + mining.bits_to_work(bits)
     wall_time = states[-1].wall_time + mining.IDEAL_BLOCK_TIME
-    if n < forward_blocks:
-      timestamp = states[-1].timestamp + total_jump // forward_blocks
-    else:
-      timestamp = states[-1].timestamp + 1
+    timestamp = selector(states, n)
     states.append(mining.State(states[-1].height + 1, wall_time, timestamp, bits, chainwork, 1, 0, 1, 0, 0, 0, ''))
     n += 1
     if autoend and n > total_blocks:
@@ -72,28 +76,66 @@ def fast_forward_attack(algoname, forward_blocks=1, total_blocks=500, total_jump
   return states, len(states)-1, profit, diff
 
 if __name__ == '__main__':
-  alg, f, t, j, FTL = 'cw-144', 1, 145, None, 7200
-  s, n, p, d = fast_forward_attack(alg, f, t, j, FTL, True)
-  print("Alg: %-10s fwd=%-3i tot=%-3i jump=%s, FTL=%i\tBlocks mined: %i\tEnd diff: %3.1f%%\tProfit: %3.1f%%\t" % (alg, f, t, str(j), FTL, n, d*100, p))
+  alg, f, t, FTL = 'cw-144', 1, 145, 7200
+  s, n, p, d = simulate_attack(alg, fast_forward_selector, f, t, FTL, True)
+  print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t" % (alg, f, t, FTL, n, d*100, p))
 
-  alg, f, t, j, FTL = 'asert-144', 1, 283, None, 7200
-  s, n, p, d = fast_forward_attack(alg, f, t, j, FTL)
-  print("Alg: %-10s fwd=%-3i tot=%-3i jump=%s, FTL=%i\tBlocks mined: %i\tEnd diff: %3.1f%%\tProfit: %3.1f%%\t" % (alg, f, t, str(j), FTL, n, d*100, p))
+  alg, f, t, FTL = 'asert-144', 1, 283, 7200
+  s, n, p, d = simulate_attack(alg, fast_forward_selector, f, t, FTL)
+  print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t" % (alg, f, t, FTL, n, d*100, p))
 
-  alg, f, t, j, FTL = 'asert-576', 1, 1290, None, 7200
-  s, n, p, d = fast_forward_attack(alg, f, t, j, FTL, True)
-  print("Alg: %-10s fwd=%-3i tot=%-3i jump=%s, FTL=%i\tBlocks mined: %i\tEnd diff: %3.1f%%\tProfit: %3.1f%%\t" % (alg, f, t, str(j), FTL, n, d*100, p))
+  alg, f, t, FTL = 'asert-576', 1, 1290, 7200
+  s, n, p, d = simulate_attack(alg, fast_forward_selector, f, t, FTL, True)
+  print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t" % (alg, f, t, FTL, n, d*100, p))
   
-  alg, f, t, j, FTL = 'wtema-144', 48, 330, None, 7200
-  s, n, p, d = fast_forward_attack(alg, f, t, j, FTL, True)
-  print("Alg: %-10s fwd=%-3i tot=%-3i jump=%s, FTL=%i\tBlocks mined: %i\tEnd diff: %3.1f%%\tProfit: %3.1f%%\t" % (alg, f, t, str(j), FTL, n, d*100, p))
-  alg, f, t, j, FTL = 'wtema-576', 75, 1200, None, 7200
-  s, n, p, d = fast_forward_attack(alg, f, t, j, FTL, True)
-  print("Alg: %-10s fwd=%-3i tot=%-3i jump=%s, FTL=%i\tBlocks mined: %i\tEnd diff: %3.1f%%\tProfit: %3.1f%%\t" % (alg, f, t, str(j), FTL, n, d*100, p))
+  alg, f, t, FTL = 'wtema-144', 48, 330, 7200
+  s, n, p, d = simulate_attack(alg, fast_forward_selector, f, t, FTL, True)
+  print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t" % (alg, f, t, FTL, n, d*100, p))
+  alg, f, t, FTL = 'wtema-576', 75, 1200, 7200
+  s, n, p, d = simulate_attack(alg, fast_forward_selector, f, t, FTL, True)
+  print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t" % (alg, f, t, FTL, n, d*100, p))
+
+  alg, f, t, FTL = 'grasberg-neutral-288', 1, 804, 7200
+  s, n, p, d = simulate_attack(alg, fast_forward_selector, f, t, FTL, False)
+  print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t" % (alg, f, t, FTL, n, d*100, p))
+  alg, f, t, FTL = 'grasberg-nodrift-288', 1, 804, 7200
+  s, n, p, d = simulate_attack(alg, fast_forward_selector, f, t, FTL, False)
+  print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t" % (alg, f, t, FTL, n, d*100, p))
+  alg, f, t, FTL = 'aserti3-288', 1, 804, 7200
+  s, n, p, d = simulate_attack(alg, fast_forward_selector, f, t, FTL, False)
+  print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t" % (alg, f, t, FTL, n, d*100, p))
+
+   # optimum attack not yet found, but here's something kinda close
+  alg, f, t, FTL = 'lwma-144', 1, 123, 7200
+  s, n, p, d = simulate_attack(alg, fast_forward_selector, f, t, FTL, True)
+  print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t" % (alg, f, t, FTL, n, d*100, p))
+
+
 
   print()
-  # optimum attack not yet found, but here's something kinda close
-  alg, f, t, j, FTL = 'lwma-144', 1, 100, None, 7200
-  for t in range(120, 125, 1):
-    s, n, p, d = fast_forward_attack(alg, f, t, j, FTL, True)
-    print("Alg: %-10s fwd=%-3i tot=%-3i jump=%s, FTL=%i\tBlocks mined: %i\tEnd diff: %3.1f%%\tProfit: %3.1f%%\t" % (alg, f, t, str(j), FTL, n, d*100, p))
+
+  alg, f, t, FTL = 'grasberg-nodrift-288', 1, 804, 7200
+  for t in range(400, 1200, 100):
+    s, n, p, d = simulate_attack(alg, fast_forward_selector, f, t, FTL, False)
+    print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t" % (alg, f, t, FTL, n, d*100, p))
+
+  print()
+  alg, f, t, FTL = 'grasberg-neutral-288', 1, 804, 7200
+  for t in range(400, 1200, 100):
+    s, n, p, d = simulate_attack(alg, two_week_jump_selector, f, t, FTL, False)
+    print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t (two-week jumper)" % (alg, f, t, FTL, n, d*100, p))
+  alg, f, t, FTL = 'grasberg-nodrift-288', 1, 804, 7200
+  for t in range(400, 1200, 100):
+    s, n, p, d = simulate_attack(alg, two_week_jump_selector, f, t, FTL, False)
+    print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t (two-week jumper)" % (alg, f, t, FTL, n, d*100, p))
+
+  print()
+  alg, f, t, FTL = 'grasberg-neutral-288', 1, 800, 7200
+  s, n, p, d = simulate_attack(alg, two_week_jump_selector, f, t, FTL, True)
+  print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t (two-week jumper)" % (alg, f, t, FTL, n, d*100, p))
+  alg, f, t, FTL = 'grasberg-nodrift-288', 1, 800, 7200
+  s, n, p, d = simulate_attack(alg, two_week_jump_selector, f, t, FTL, True)
+  print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t (two-week jumper)" % (alg, f, t, FTL, n, d*100, p))
+  alg, f, t, FTL = 'aserti3-288', 1, 800, 7200
+  s, n, p, d = simulate_attack(alg, two_week_jump_selector, f, t, FTL, True)
+  print("Alg: %-20s fwd=%-3i tot=%-4i FTL=%i\tBlocks mined: %i\tEnd diff: %4.2f%%\tProfit: %4.2f%%\t (two-week jumper)" % (alg, f, t, FTL, n, d*100, p))
